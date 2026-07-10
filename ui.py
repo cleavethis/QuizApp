467e692c83b13fe48c70b91dcd18dd8d79208a8d
Rick <111731365+cleavethis@users.noreply.github.com>
2026-07-10 14:46:57 -0700

    add logic to fetch next question to gui

--- ui.py
+++ ui.py
@@ -1,10 +1,15 @@
 from tkinter import *
 
+from quiz_brain import QuizBrain
+from data import question_data
+import random as rd
+
 THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.score = 0
         self.window = Tk()
         self.window.title("Quizle")
@@ -14,7 +19,14 @@ class QuizInterface:
         self.score_label.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=250, bg="white")
-        self.question_text = self.canvas.create_text(150, 125, text="Question", fill=THEME_COLOR, font=("Arial", 20, "italic"))
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            text="question",
+            fill=THEME_COLOR,
+            font=("Arial", 20, "italic"),
+            width=250)
+
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         true_image = PhotoImage(file="./images/true.png")
@@ -25,12 +37,12 @@ class QuizInterface:
         self.false_button = Button(image=false_image, highlightthickness=0)
         self.false_button.grid(row=2, column=1)
 
-
-
-
-
-
-
+        self.get_next_question()
 
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
 
